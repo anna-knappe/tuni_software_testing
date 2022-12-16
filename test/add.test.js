74ee092fecc45559d88b0ab4ae41211110407bc1
Anna Knappe <anna.knappe@sanoma.com>
2022-12-16 23:32:48 +0200

    Add mocha test for add.js

---
+++ test/add.test.js
@@ -0,0 +1,10 @@
+import chai from "chai";
+import add from "../src/add.js";
+
+const expect = chai.expect
+
+describe('Addition', () => {
+		it('1 + 1 should equal 2', () => {
+			expect(add(1, 1)).to.equal(2)
+		})
+})
