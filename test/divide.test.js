c486b59be7cc5827ae416e36426052da54e001d9
Anna Knappe <anna.knappe@sanoma.com>
2022-12-16 23:53:29 +0200

    Add test for divide.js

---
+++ test/divide.test.js
@@ -0,0 +1,10 @@
+import chai from "chai";
+import divide from "../src/divide.js";
+
+const expect = chai.expect
+
+describe('Division', () => {
+		it('6 divided by 4 equals 1.5', () => {
+			expect(divide(6, 4)).to.equal(1.5)
+		})
+})
