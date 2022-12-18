5ef65c75792c3aa16ee650e48ae254c366e6ca90
Sonja Koivu <sonja.m.koivu@tuni.fi>
2022-12-18 20:03:46 +0200

    Added tests for null and NaN -cases ans different numbers

--- test/defaultTo.test.js
+++ test/defaultTo.test.js
@@ -7,7 +7,16 @@ describe('defaultTo', () => {
 		it("defaultTo(1, 10) => 1", () => {
 			expect(defaultTo(1, 10)).to.equal(1)
 		})
-		it("defaultTo(undefined, 10) => undefined", () => {
+		it("defaultTo(undefined, 10) => 10", () => {
 			expect(defaultTo(undefined, 10)).to.equal(10)
+		})
+		it("defaultTo(null, -10) => -10", () => {
+			expect(defaultTo(null, -10)).to.equal(-10)
+		})
+		it("defaultTo(NaN, -999.9) => -999.9", () => {
+			expect(defaultTo(NaN, -999.9)).to.equal(-999.9)
+		})
+		it("defaultTo(0, 200) => 0", () => {
+			expect(defaultTo(0, 200)).to.equal(0)
 		})
 });
