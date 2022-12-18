bebe439a4ebd1321cd6d1534144f1c4e2d52a88e
Anna Knappe <anna.knappe@sanoma.com>
2022-12-18 20:53:37 +0200

    Add back failing tests

--- test/chunk.test.js
+++ test/chunk.test.js
@@ -4,10 +4,10 @@ import chunk from "../src/chunk.js";
 const expect = chai.expect
 
 describe('Chunk', () => {
-		//it("['a', 'b', 'c', 'd'] should become ['a', 'b'], ['c', 'd']", () => {
-		//	expect(chunk(['a', 'b', 'c', 'd'], 2)).to.deep.equal([['a', 'b'], ['c', 'd']])
-		//})
-		//it("['a', 'b', 'c', 'd'] should become ['a', 'b', 'c'], ['d']", () => {
-		//	expect(chunk(['a', 'b', 'c', 'd'], 3)).to.deep.equal([['a', 'b', 'c'], ['d']])
-		//})
+		it("['a', 'b', 'c', 'd'] should become ['a', 'b'], ['c', 'd']", () => {
+			expect(chunk(['a', 'b', 'c', 'd'], 2)).to.deep.equal([['a', 'b'], ['c', 'd']])
+		})
+		it("['a', 'b', 'c', 'd'] should become ['a', 'b', 'c'], ['d']", () => {
+			expect(chunk(['a', 'b', 'c', 'd'], 3)).to.deep.equal([['a', 'b', 'c'], ['d']])
+		})
 });
--- test/toString.test.js
+++ test/toString.test.js
@@ -4,13 +4,13 @@ import toString from "../src/toString.js";
 const expect = chai.expect
 
 describe('toString', () => {
-		//it('null toString -> ', () => {
-		//	expect(toString(null)).to.equal('')
-		//})
-        //it('-0 toString-> -0', () => {
-		//	expect(toString(-0)).to.equal('-0')
-		//})
-        //it('[1, 2, 3] toString -> [1, 2, 3]', () => {
-		//	expect(toString([1, 2, 3])).to.equal('1,2,3')
-		//})
+		it('null toString -> ', () => {
+			expect(toString(null)).to.equal('')
+		})
+        it('-0 toString-> -0', () => {
+			expect(toString(-0)).to.equal('-0')
+		})
+        it('[1, 2, 3] toString -> [1, 2, 3]', () => {
+			expect(toString([1, 2, 3])).to.equal('1,2,3')
+		})
 });
