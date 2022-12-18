198b165c70e6850d398792967584087f6e6f37d5
Sonja Koivu <sonja.m.koivu@tuni.fi>
2022-12-18 20:27:10 +0200

    Added tests for bigger array and an empty array

--- test/filter.test.js
+++ test/filter.test.js
@@ -5,10 +5,23 @@ const expect = chai.expect
 
 const users = [
        { 'user': 'barney', 'active': true },
-       { 'user': 'fred',   'active': false }];
+       { 'user': 'fred',   'active': false },
+       { 'user': 'betty',   'active': false },
+       { 'user': 'wilma',   'active': false }];
+
+const noUsers = []
 
 describe('Filter', () => {
 		it("Filters active users", () => {
 			expect(filter(users, ({ active }) => active)).to.deep.equal([{ 'user': 'barney', 'active': true }])
+		})
+		it("Filters inactive users", () => {
+			expect(filter(users, ({ active }) => !active)).to.deep.equal([
+				{ 'user': 'fred',   'active': false },
+				{ 'user': 'betty',   'active': false },
+				{ 'user': 'wilma',   'active': false }])
+		})
+		it("Returns an empty array when given an empty array", () => {
+			expect(filter(noUsers, ({ active }) => active)).to.deep.equal([[]])
 		})
 });
